441683bd92704012b317dd3e25ee5a8814726efa
Danny Molares <daniel.molares8@gmail.com>
2024-12-03 00:11:15 -0500

    Minor changes to layout

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
         Hello world! This will be my new portfolio! (Currently building...)
       </h1>
       <br/>
-      <div class="columns-3">
+      <div>
+
+      </div>
+      <div class="bg-fixed bg-img"></div>
+      {/* <div class="columns-3">
         <img src="https://source.unsplash.com/random/200x200?sig=1" alt="random" />
         <img src="https://source.unsplash.com/random/200x200?sig=2" alt="random"/>
         <img src="https://source.unsplash.com/random/200x200?sig=3" alt="random"/>
-      </div>
+      </div> */}
       </header>
     </div>
   );
